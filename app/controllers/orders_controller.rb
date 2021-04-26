179ad61b6ae44a0f5bf777134d3e30a4fdd0d54a
raytakekawa <leiluckyb@gmail.com>
2021-04-26 15:41:35 +0900

    修正2

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_item
-  before_action :move_to_index, except: :index
+  before_action :move_to_index
 
   def index
     @order_destination = OrderDestination.new
@@ -30,7 +30,7 @@ class OrdersController < ApplicationController
 
   def move_to_index
     if @item.user.id == current_user.id || @item.order.present?
-      redirect_to action: :index
+      redirect_to root_path
     end
   end
 
